c51de424a15bf7096cdb5c4f429b350317046a18
gra <graciqueira@gmail.com>
2020-03-12 14:50:07 -0300

    mosaico

--- forest-monitor-app/src/app/pages/explore/map/layers/base-layers.in-memory.ts
+++ forest-monitor-app/src/app/pages/explore/map/layers/base-layers.in-memory.ts
@@ -1,6 +1,7 @@
 import { tileLayer } from 'leaflet';
 import { BdcLayer } from './layer.interface';
 
+
 /**
  * return a list of external base layers/maps
  * static WMS list
@@ -54,6 +55,55 @@ export const BaseLayers: BdcLayer[] = [
          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       }),
    },
+   /*{
+      id: 'mosaico',
+      enabled: false,
+      name: 'MOSAICO 2018',
+      layer: tileLayer('http://terrabrasilis.dpi.inpe.br/geoserver/ows?service=WMS&
+      request=GetMap&layers=prodes-legal-amz%3Atemporal_mosaic_legal_amazon_2000_2018&
+      styles=&format=image%2Fpng&transparent=true&version=1.1.1&tiled=true&_name=temporal_mosaic_legal_amazon_2000_2018&_baselayer=false&
+      time=2019-01-01T00%3A00%3A00.000Z&width=256&height=256&srs=EPSG%3A3857&bbox=-6261721.357121639,-1252344.2714243263,-5009377.085697311,0',{
+         attribution: "Weather data © 2012 IEM Nexrad"
+     
+       }),
+      
+   },*/
+   {
+      id: 'mosaico',
+      enabled: false,
+      name: 'MOSAICO 2017',
+      layer: tileLayer.wms(`http://terrabrasilis.dpi.inpe.br/geoserver/wms`, {
+         layers: `prodes-legal-amz:temporal_mosaic_legal_amazon_2000_2018`,
+         format: 'image/png',
+         transparent: true,
+         className: `MOSAICO_landsat`,
+         time:'2017-01-01T00:00:00.000Z'       
+     } as any)
+   },
+   {
+      id: 'mosaico',
+      enabled: false,
+      name: 'MOSAICO 2018',
+      layer: tileLayer.wms(`http://terrabrasilis.dpi.inpe.br/geoserver/wms`, {
+         layers: `prodes-legal-amz:temporal_mosaic_legal_amazon_2000_2018`,
+         format: 'image/png',
+         transparent: true,
+         className: `MOSAICO_landsat`,
+         time:'2018-01-01T00:00:00.000Z'       
+     } as any)
+   },
+   {
+      id: 'mosaico',
+      enabled: false,
+      name: 'MOSAICO 2019',
+      layer: tileLayer.wms(`http://terrabrasilis.dpi.inpe.br/geoserver/wms`, {
+         layers: `prodes-legal-amz:temporal_mosaic_legal_amazon_2000_2018`,
+         format: 'image/png',
+         transparent: true,
+         className: `MOSAICO_landsat`,
+         time:'2019-01-01T00:00:00.000Z'       
+     } as any)
+   },
    {
       id: '',
       enabled: false,
@@ -61,3 +111,6 @@ export const BaseLayers: BdcLayer[] = [
       layer: tileLayer('', {})
    }
 ];
+function newFunction(): any {
+   return 'image/png';
+}
