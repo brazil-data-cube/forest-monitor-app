b238c3250c03f4ffa4f140e265306dbb3b4e328d
Beto Noronha <beto_noronha@live.com>
2020-06-01 13:07:33 -0300

    build without base path

--- forest-monitor-app/src/app/app.module.ts
+++ forest-monitor-app/src/app/app.module.ts
@@ -11,13 +11,11 @@ import { AuthModule } from './pages/auth/auth.module';
 import * as fromApp from './app.reducer';
 import * as fromAuth from './pages/auth/auth.reducer';
 import * as fromExplore from './pages/explore/explore.reducer';
-import { APP_BASE_HREF } from '@angular/common';
 
 /**
  * Initial Module of Application (SPA)
  */
 @NgModule({
-  providers: [{provide: APP_BASE_HREF, useValue: '/forest-monitor/'}],
   declarations: [
     AppComponent
   ],
