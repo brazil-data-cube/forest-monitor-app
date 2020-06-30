c541dfc568a6d4b32d120f8d234a44de13d1a658
Claudio Henrique Bogossian <claudio.bogossian@gmail.com>
2020-06-30 10:35:03 -0300

    Adding base path parameter, to be a dynamic path

--- forest-monitor-app/src/app/app-routing.module.ts
+++ forest-monitor-app/src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ import { ExploreComponent } from './pages/explore/explore.component';
 const routes: Routes = [
   { path: 'explore', component: ExploreComponent },
   { path: 'auth', loadChildren: './pages/auth/auth.module#AuthModule'},
-  { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
-  { path: '**', redirectTo: '/explore', pathMatch: 'full' }
+  { path: '*', redirectTo: 'auth/login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'explore', pathMatch: 'full' }
 ];
 
 /**
