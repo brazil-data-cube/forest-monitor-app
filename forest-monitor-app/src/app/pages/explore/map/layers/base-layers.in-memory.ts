ef03065a29eb840de3b86ce43193877e5fd28afc
gracielle-ch <graciqueira@gmail.com>
2021-02-04 11:43:29 -0300

    Add layer Planet

--- forest-monitor-app/src/app/pages/explore/map/layers/base-layers.in-memory.ts
+++ forest-monitor-app/src/app/pages/explore/map/layers/base-layers.in-memory.ts
@@ -1,3 +1,4 @@
+import { formatDateUSA } from 'src/app/shared/helpers/date';
 
 import { tileLayer } from 'leaflet';
 import { BdcLayer } from './layer.interface';
@@ -12,30 +13,37 @@ export class BaseLayers  {
    public static getBdcLayer(BdcLayer) { 
       let data = new Date();
       let month = data.getMonth() +1 ;
-      let str_Month;
-      if (month < 10) {
-         str_Month = '0' + month;
-      } else {
-         str_Month = month.toString();
-         }
       let year = data.getFullYear(); 
-      let str_data;
-      let str_data_old;
-      if (str_Month > 1 && str_Month != 2){
-         str_data = year + '-' + (str_Month-1) ;   
-         str_data_old = year + '-' + (str_Month-2)
-      }
-      else if (str_Month == 1) {
-         str_data = (year -1) + '-' + 12 ;
-         str_data_old = (year -1) + '-' + 11 ;
-         }
-      else if (str_Month == 2) {
-         str_data = year + '-' + (str_Month-1) ;
-         str_data_old = (year -1) + '-' + 12 ;
-         }
-      
 
-      
+      let strMonth;
+      let strmonthOld;
+      let strData;
+      let strDataOld;
+              
+      if (month > 1 && month != 2){
+         month = month - 1 ;
+         strmonthOld = month - 2;
+         if (month < 10 || strmonthOld < 10) {
+            strMonth = '0' + month;
+            
+   
+         } else {
+            strMonth = month.toString();
+         }
+         
+         strData = year + '-' +  strMonth; 
+         strDataOld = year + '-' + strMonth;
+      }
+      else if (month == 1) {
+         strData = (year -1) + '-' + 12 ;
+         strDataOld = (year -1) + '-' + 11 ;
+         }
+      else if (month == 2) {
+         month = month - 1 ;
+         strData = year + '-' +   '0' + month;
+         strDataOld = (year -1) + '-' + 12 ;
+      }
+
       return  BdcLayer  = [
          {
             id: 'google_sattelite',
@@ -126,15 +134,8 @@ export class BaseLayers  {
          {        
             id: 'planet',
             enabled: false,
-            name: `PLANET ${str_data_old}`,
-            layer: tileLayer(`https://tiles.planet.com/basemaps/v1/planet-tiles/planet_medres_normalized_analytic_${str_data_old}_mosaic/gmap/{z}/{x}/{y}.png?api_key=12b17c3548c047218485084e2f8c8048`)
-         },
-         {
-            
-            id: 'planet',
-            enabled: false,
-            name: `PLANET ${str_data}`,
-            layer: tileLayer(`https://tiles.planet.com/basemaps/v1/planet-tiles/planet_medres_normalized_analytic_${str_data}_mosaic/gmap/{z}/{x}/{y}.png?api_key=12b17c3548c047218485084e2f8c8048`)
+            name: `PLANET ${strDataOld} até ${strData}`,
+            layer: tileLayer(`https://tiles.planet.com/basemaps/v1/planet-tiles/planet_medres_normalized_analytic_${strDataOld}_mosaic/gmap/{z}/{x}/{y}.png?api_key=12b17c3548c047218485084e2f8c8048`)
          }
             
       ];
