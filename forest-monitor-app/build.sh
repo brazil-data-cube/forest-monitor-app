40c76504ce875ba9938520af9640d1a7d9f5cf54
ammaciel <adelinemaciel22@gmail.com>
2022-03-18 15:44:14 -0300

    Configure package.json

--- forest-monitor-app/build.sh
+++ forest-monitor-app/build.sh
@@ -6,10 +6,6 @@ echo ""
 
 VERSION=$(cat package.json | grep -oP '(?<="version": ")[^"]*')
 
-# docker build -t forestmonitor/forest-monitor-frontend:v$VERSION .
+docker build -t forestmonitor/forest-monitor-frontend:v$VERSION .
 
-# docker push forestmonitor/forest-monitor-frontend:v$VERSION
-
-docker build -t ammaciel/forest-monitor-frontend:v$VERSION .
-
-docker push ammaciel/forest-monitor-frontend:v$VERSION
+docker push forestmonitor/forest-monitor-frontend:v$VERSION
